359214e4d491ca98695362b5065cee9d546f36c1
Algorithms <142917637+Algo1337@users.noreply.github.com>
2026-01-05 06:43:19 -0600

    testing

--- load.c
+++ load.c
@@ -1,24 +1,13 @@
-#include <stdio.h>
-#include <stddef.h>
-#include <stdio.h>
+#undef _STDIO_H
 
-#if defined(__x86__)
-        #define SYSCALL_REGISTER SYSCALL_REGISTER_x86
-        #define EXECUTE_SYSCALL EXECUTE_SYSCALL_x86
-#elif defined(__x86_64__)
-        #define SYSCALL_REGISTER SYSCALL_REGISTER_x86
-        #define EXECUTE_SYSCALL EXECUTE_SYSCALL_x86_64
-#endif
-
-long __test(long arg1, long arg2, long arg3, long arg4, long arg5, long arg6, long sys)
-{
-        register long ss asm(SYSCALL_REGISTER) = sys;
-        asm(EXECUTE_SYSCALL);
+void *print_(int fd, char *v, int sz, int sys) {
+	register long b asm("rax") = sys;
+	asm("syscall");
 }
 
-int main()
+void _start()
 {
-        __test(1, (long)"gay\n", 4, 0, 0, 0, 1);
-        return 0;
+		print_(1, "dick\n", 5, 1);
+		print_(0, 0, 0, 60);
 }
 
